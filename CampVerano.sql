47b5984c985af684b901ff70fb9b8df27d69bbfd
EdManDev <ed.manigat@gmail.com>
2020-04-13 18:43:43 -0400

    16 tables

--- CampVerano.sql
+++ CampVerano.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
 -- version 4.9.3
 -- https://www.phpmyadmin.net/
-
+--
 -- Host: localhost:8889
--- Generation Time: Mar 11, 2020 at 09:02 PM
+-- Generation Time: Apr 13, 2020 at 08:46 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -17,16 +17,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tableCamp`
+--
+
+CREATE TABLE `tableCamp` (
+  `_idCamp` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `nombre_Camp` varchar(200) ,
+  `desc_Camp` varchar(200) ,
+  `REGISTRO` int(10),
+  `dates_Camp` varchar(200) 
+) CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tableCiudad`
 --
 
 CREATE TABLE `tableCiudad` (
-  `_idCiudad` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `nombre_Ciudad` varchar(200),
-  `nombre_Sector` varchar(200),
-  `desc_Ciudad` varchar(200),
-  `pobla_Ciudad` varchar(200)
-)CHARSET=utf8;
+  `_idCiudad` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `nombre_Ciudad` varchar(200) ,
+  `nombre_Sector` varchar(200) ,
+  `desc_Ciudad` varchar(200) ,
+  `pobla_Ciudad` varchar(200) 
+) CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -35,12 +49,12 @@ CREATE TABLE `tableCiudad` (
 --
 
 CREATE TABLE `tableNinio` (
-  `_idNinio` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `nombre` varchar(200),
-  `apellido` varchar(200),
-  `sexo` varchar(200),
-  `fech_Nac` date
-)CHARSET=utf8;
+  `_idNinio` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `nombre` varchar(200) ,
+  `apellido` varchar(200) ,
+  `sexo` varchar(200) ,
+  `fech_Nac` date 
+) CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -49,12 +63,12 @@ CREATE TABLE `tableNinio` (
 --
 
 CREATE TABLE `tablePadres` (
-  `_idPadres` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `nombre` varchar(200),
-  `apellido` varchar(200),
-  `sexo` varchar(200),
-  `fech_Naci` varchar(200)
-)CHARSET=utf8;
+  `_idPadres` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `nombre` varchar(200) ,
+  `apellido` varchar(200) ,
+  `sexo` varchar(200) ,
+  `fech_Naci` varchar(200) 
+) CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -63,27 +77,12 @@ CREATE TABLE `tablePadres` (
 --
 
 CREATE TABLE `tablePais` (
-  `_idPais` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `nombre_Pais` varchar(200),
-  `nombre_Ciudad` varchar(200),
-  `desc_Pais` varchar(200),
-  `pobla_Pais` varchar(200)
-)CHARSET=utf8;
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tablePais`
---
-
-CREATE TABLE `tableCamp` (
-  `_idCamp` int(10),
-  `nombre_Camp` varchar(200),
-  `desc_Camp` varchar(200),
-  `REGISTRO` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `dates_Camp` varchar(200)
-)CHARSET=utf8;
+  `_idPais` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `nombre_Pais` varchar(200) ,
+  `nombre_Ciudad` varchar(200) ,
+  `desc_Pais` varchar(200) ,
+  `pobla_Pais` varchar(200) 
+) CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -92,55 +91,102 @@ CREATE TABLE `tableCamp` (
 --
 
 CREATE TABLE `tablePersona` (
-  `_idPer` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `cant_Ninio` varchar(200),
-  `cant_Profesor` varchar(200),
-  `tel` varchar(200),
-  `dates` date
-)CHARSET=utf8;
+  `_idPer` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cant_Ninio` varchar(200) ,
+  `cant_Profesor` varchar(200) ,
+  `tel` varchar(200) ,
+  `dates` date 
+) CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tableTelCli`
+-- Table structure for table `tableTel_Cli`
 --
 
 CREATE TABLE `tableTel_Cli` (
-  `_idCli` int(10) ,
-  `_idTipo_Tel` int(10),
-  `NUMERO` int(50),
-  `REGISTRO` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `telCli` int(10)
-)CHARSET=utf8;
+  `_idCli` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `_idTipo_Tel` int(10) ,
+  `NUMERO` int(50) ,
+  `REGISTRO2` int(10),
+  `telCli` int(10) 
+) CHARSET=utf8;
 
-
--- --------------------------------------------------------
 
 --
--- Table structure for table `tableTipo_Tels`
+-- Table structure for table `tableTel_Cli`
 --
 
-CREATE TABLE `tableTipo_Tel` (
-  `_idTel`int(10) PRIMARY KEY AUTO_INCREMENT,
-  `celular` varchar(200),
-  `trabajo` varchar(200),
-  `residencial` varchar(200),
-  `publico` varchar(200)
-)CHARSET=utf8;
+CREATE TABLE `tableEmpleado` (
+  `_idemp` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_empl` int(10) ,
+  `nom_empl` int(50) ,
+  `ap_empl` int(10),
+  `fech_empl` int(10) 
+) CHARSET=utf8;
 
--- --------------------------------------------------------
+CREATE TABLE `tableEmpleadoCop` (
+  `_idemp` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_empl` int(10) ,
+  `nom_empl` int(50) ,
+  `ap_empl` int(10),
+  `fech_empl` int(10) 
+) CHARSET=utf8;
 
---
--- Table structure for table `tableSector`
---
+CREATE TABLE `tableEmpleadoCop2` (
+  `_idemp2` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_empl2` int(10) ,
+  `nom_empl2` int(50) ,
+  `ap_empl2` int(10),
+  `fech_empl2` int(10) 
+) CHARSET=utf8;
 
-CREATE TABLE `tableSector` (
-  `_idSector` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `tipo_Sangre` varchar(200),
-  `num_Plaza` varchar(200),
-  `direccion` varchar(200),
-  `zona` varchar(200)
-)CHARSET=utf8;
+CREATE TABLE `tableEmpleadoNorm` (
+  `_idemp3` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_empl4` int(10) ,
+  `nom_empl5` int(50) ,
+  `ap_empl6` int(10),
+  `fech_empl7` int(10) 
+) CHARSET=utf8;
 
 
+CREATE TABLE `tableOperaciones` (
+  `_idop` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_op` int(10) ,
+  `nom_op` int(50) ,
+  `ap_op` int(10),
+  `fech_op` int(10) 
+) CHARSET=utf8;
 
+CREATE TABLE `tableOperacionesPub` (
+  `_idop` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_pub` int(10) ,
+  `nom_oppub` int(50) ,
+  `ap_oppub` int(10),
+  `fech_oppub` int(10) 
+) CHARSET=utf8;
+
+CREATE TABLE `tableEmpleadoNorm2` (
+  `_idemp3` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_empl4` int(10) ,
+  `nom_empl5` int(50) ,
+  `ap_empl6` int(10),
+  `fech_empl7` int(10) 
+) CHARSET=utf8;
+
+
+CREATE TABLE `tableOperacionesNiv1` (
+  `_idop` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_op2` int(10) ,
+  `nom_op2` int(50) ,
+  `ap_op22` int(10),
+  `fech_op2` int(10) 
+) CHARSET=utf8;
+
+CREATE TABLE `tableOperacionesPubNiv2` (
+  `_idop` int(10) PRIMARY KEY  AUTO_INCREMENT,
+  `cod_pub2` int(10) ,
+  `nom_oppub2` int(50) ,
+  `ap_oppub2` int(10),
+  `fech_oppub2` int(10) 
+) CHARSET=utf8;
