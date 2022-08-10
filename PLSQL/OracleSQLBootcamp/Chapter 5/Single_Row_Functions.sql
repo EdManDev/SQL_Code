ad91899de2afc0a0e71010ea84263ced1ef743a8
EdManDev <ed.manigat@gmail.com>
2022-08-09 23:27:09 -0400

    arr

--- PLSQL/OracleSQLBootcamp/Chapter 5/Single_Row_Functions.sql
+++ PLSQL/OracleSQLBootcamp/Chapter 5/Single_Row_Functions.sql
@@ -4,20 +4,28 @@
 -- department number for all employees whose name starts with the letters “A” Give each 
 -- column an appropriate column alias. Sort the results by the employees’ last names
 
-SELECT last_name, first_name,
+SELECT 
+  last_name, 
+  first_name,
 CONCAT (CONCAT (last_name, ' '), first_name) as "Full Name"
-FROM employees
-WHERE department_id>=110;
+FROM 
+  employees
+WHERE 
+  department_id>=110;
 
 -- 2. Edit the previous query so that the end user is prompted to enter a letter that the last 
 -- name starts with. For example, if the user enters “K” when prompted for a letter, the 
 -- output should show all employees whose last name starts with the letter “K.”
 
-SELECT last_name, SUBSTR (last_name,1,4) "Partial",
-SUBSTR (last_name, -2, 2) "Last two char",
-LENGTH (last_name) "Name Length", INSTR (last_name, 's') "Contain's S"
-FROM employees
-WHERE department_id>=110;
+SELECT 
+  last_name,
+  SUBSTR (last_name,1,4) "Partial",
+  SUBSTR (last_name, -2, 2) "Last two char",
+  LENGTH (last_name) "Name Length", INSTR (last_name, 's') "Contain's S"
+FROM 
+  employees
+WHERE 
+  department_id>=110;
 
 --------------------------------------------------------------------------------------------------------
 
@@ -28,17 +36,32 @@ WHERE department_id>=110;
 -- located in location 1700, give each column an appropriate column alias and sort the 
 -- output by the length of department_name in descending order
 
+SELECT 
+  department_id, 
+  department_name, 
+  length(department_name) "Length", 
+  substr(department_name,-4,4) 
+AS "Cutted String"
+FROM 
+  departments
+WHERE 
+  location_id=1700
+ORDER BY 3 desc
+
 -- 2. Write a query to display the country number, country name, country name after 
 -- replacing the first two characters with ‘**’, give each column an appropriate column 
 -- alias
 
-SELECT last_name,
-LPAD (last_name, 10,'*') "LPAD",
-RPAD (last_name, 10,'*') "RPAD",
-TRIM(' Test String') "TRIM",
-REPLACE (last_name, 'gg','ff') "REPLACE"
-FROM employees
-WHERE department_id>=110;
+SELECT 
+  last_name,
+  LPAD (last_name, 10,'*') "LPAD",
+  RPAD (last_name, 10,'*') "RPAD",
+  TRIM(' Test String') "TRIM",
+  REPLACE (last_name, 'gg','ff') "REPLACE"
+FROM 
+  employees
+WHERE 
+  department_id>=110;
 
 --------------------------------------------------------------------------------------------------------
 
