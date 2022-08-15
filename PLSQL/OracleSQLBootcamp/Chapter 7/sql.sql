858cf6bea624ec2cdbf37915bfd8f4c30005d6b0
EdManDev <ed.manigat@gmail.com>
2022-08-15 14:21:15 -0400

    chapter 7 OK

--- PLSQL/OracleSQLBootcamp/Chapter 7/sql.sql
+++ PLSQL/OracleSQLBootcamp/Chapter 7/sql.sql
@@ -35,22 +35,111 @@ WHERE
 --------------------------------------------------------------------------------------------------------
 -- Chapter 7 practice 2 for chapter 7 lesson 2
 -- 1. Write a query to display number of distinct departments in employees table
+
+SELECT
+  count(DISTINCT department_id)
+FROM 
+  employees
+
 -- 2. Write a query to display number of distinct locations in departments table
--- 3. Write a query to display number of managers, provide an appropriate column alias for
--- the column
+
+SELECT
+  count(DISTINCT location_id)
+FROM 
+  departments
+
+-- 3. Write a query to display number of managers, provide an appropriate column alias for the column
+
+SELECT
+  count(DISTINCT manager_id)
+FROM 
+  employees
 --------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------
 -- Chapter 7 practice 3 for chapter 7 lesson 3
 -- 1. Write a query to display number of employees with the same job, give an appropriate
 -- column alias and sort the output by number of employees in descending order
+
+SELECT 
+  job_id "JOB", 
+  count(*) "No of Employees"
+FROM 
+  employees
+GROUP BY 
+  job_id
+ORDER BY 2 desc
+
 -- 2. Write a query to display number of departments in each location.
+SELECT
+  department_id,
+  location_id
+FROM
+  departments
+--------------------------
+SELECT
+  location_id, count(*)
+FROM
+  departments
+GROUP BY 
+  location_id
+
 -- 3. Write a query to display sum, average, min, max salary per each department and job
+SELECT 
+  job_id,
+  department_id,
+  sum(salary),
+  min(salary),
+  max(salary),
+  round(avg(salary),0)
+FROM
+  employees
+GROUP BY
+  job_id,
+  department_id
+ORDER BY 2,1
 --------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------
 -- Chapter 7 practice 4 for chapter 7 lesson 4
--- 1. Write a query to display regions’ IDs having more than 5 countries, order the output by
--- number of countries
--- 2. Write a query to display the sum of salaries of employees per each job for departments
--- number (50, 80) , limit the output for only rows where the sum of salaries greater than
--- or equal to 60000
+-- 1. Write a query to display regions’ IDs having more than 5 countries, order the output by number of countries
+
+SELECT
+  region_id,
+  count(*)
+FROM
+  countries
+GROUP BY
+  region_id
+HAVING BY count(*)>5
+ORDER BY 2
+
+-- 2. Write a query to display the sum of salaries of employees per each job for departments number (50, 80),
+
+SELECT 
+  job_id,
+  sum(salary)
+FROM
+  employees
+WHERE
+  department_id 
+IN 
+  (50,80)
+GROUP BY 
+  job_id
+
+--------------------- limit the output for only rows where the sum of salaries greater than or equal to 60000
+
+SELECT 
+  job_id,
+  sum(salary)
+FROM
+  employees
+WHERE
+  department_id 
+IN 
+  (50,80)
+GROUP BY 
+  job_id
+HAVING 
+  sum(salary)>60000
+
 --------------------------------------------------------------------------------------------------------
